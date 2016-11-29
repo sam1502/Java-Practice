38d8c0573d92b8bbe1738b9a9d4beb28689af116
Sameer Neema <sam1502@users.noreply.github.com>
2016-11-28 21:38:04 -0500

    Add files via upload

---
+++ Codes/BracketChecking.java
@@ -0,0 +1,43 @@
+//Given  strings of brackets, determine whether each sequence of brackets is balanced.
+//If a string is balanced, print true on a new line; otherwise, print False on a new line.
+
+package stringpractice;
+import java.util.*;
+public class BracketChecking{
+public static boolean CheckParentesis(String str)
+{
+    if (str.isEmpty())
+        return true;
+
+    Stack<Character> stack = new Stack<Character>();
+    for (int i = 0; i < str.length(); i++)
+    {
+        char current = str.charAt(i);
+        if (current == '{' || current == '(' || current == '[')
+        {
+            stack.push(current);
+        }
+
+
+        if (current == '}' || current == ')' || current == ']')
+        {
+            if (stack.isEmpty())
+                return false;
+
+            char last = stack.peek();
+            if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
+                stack.pop();
+            else 
+                return false;
+        }
+
+    }
+
+    return stack.isEmpty();
+	}
+
+public static void main(String[] args) {        
+    String str = "({})";
+    System.out.println(CheckParentesis(str)); 
+	}
+}
