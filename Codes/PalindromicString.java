6d675838a58f78de9919b8ab4e1c18d5de542ad6
Sameer Neema <sam1502@users.noreply.github.com>
2016-11-30 14:21:22 -0500

    Update PalindromicString.java

--- Codes/PalindromicString.java
+++ Codes/PalindromicString.java
@@ -1,3 +1,7 @@
+//You have been given a String S
+//You need to find and print whether this string is a palindrome or not.
+//If yes, print "YES" (without quotes), else print "NO" (without quotes).
+
 import java.util.*;
 
 class PalindromicString {
