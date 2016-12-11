820aa743008bd28c33dc316c17ed98f51c5a42fd
Sameer Neema <sam1502@users.noreply.github.com>
2016-12-11 18:21:52 -0500

    Add files via upload

---
+++ Codes/AnagramsHR.java
@@ -0,0 +1,29 @@
+//Two strings  and  are called anagrams if they consist same characters
+//but may be in different orders.
+//Java program to check if strings are anagram of each other or not.
+//Print Anagrams or Not Anagrams.
+import java.util.*;
+
+public class AnagramsHR {
+	static boolean isAnagram(String a, String b) {
+        char c[] = a.toLowerCase().toCharArray();
+        char d[] = b.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        Arrays.sort(d);
+        if(Arrays.equals(c,d)){
+            return true;
+        }
+        return false;
+    }
+public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter first String: ");
+    String a = scan.next();
+    System.out.println("Enter seconf jumbled String");
+    String b = scan.next();
+    scan.close();
+    boolean ret = isAnagram(a, b);
+    System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
