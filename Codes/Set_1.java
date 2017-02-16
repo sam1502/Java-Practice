1ba347420dee455472a1f7843f1f1468f5c956be
Sameer Neema <sam1502@users.noreply.github.com>
2017-02-16 13:38:06 -0500

    Add files via upload

---
+++ Codes/Set_1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Set_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count[] = {1,3,6,8,16,22};
+		Set<Integer> set = new HashSet<Integer>();
+		try{
+			for(int i=0;i<count.length;i++){
+				set.add(count[i]);
+			}
+			System.out.println(set);
+			
+			TreeSet<Integer> sortedSet = new TreeSet<Integer>(set);
+			System.out.println("Sorted set is: ");
+			System.out.println(sortedSet);
+			
+			System.out.println("The First Element of the Set is :"+sortedSet.first());
+			System.out.println("The Last Element of the Set is : "+sortedSet.last());
+		}
+		catch (Exception e){}
+
+	}
+
+}
