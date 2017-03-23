d8719c54e4667ba5c3cbf4d5209dbe1b7d6c1623
Sameer Neema <sam1502@users.noreply.github.com>
2017-03-23 19:12:14 -0400

    Add files via upload

---
+++ Codes/consecutivElements_Array.java
@@ -0,0 +1,58 @@
+public class consecutivElements_Array {
+	
+	int getMin(int arr[], int n){
+		int min = arr[0];
+		for(int i=0;i<n;i++){
+			if(arr[i] < min){
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+	
+	int getMax(int arr[], int n){
+		int max = arr[0];
+		for(int i=0;i<n;i++){
+			if(arr[i]>max)
+				max = arr[i];
+		}
+		return max;
+	}
+	
+	boolean areConsecutive(int arr[], int n){
+		if(n<1)
+			return false;
+		
+		int min = getMin(arr, n);
+		int max = getMax(arr, n);
+		
+		if(max - min + 1 == n){
+			boolean visited[] = new boolean[n];
+			int i;
+			for(i=0;i<n;i++){
+				if(visited[arr[i] - min] != false)
+					return false;
+				
+				visited[arr[i] - min] = true;
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		consecutivElements_Array cea = new consecutivElements_Array();
+		int arr[] = {5,6,1,3,2,4};
+		int n = arr.length;
+		
+		if(cea.areConsecutive(arr, n) == true)
+			System.out.println("Array Elements are consecutive");
+		else
+			System.out.println("Array Elements are not consecutive");
+
+	}
+
+}
