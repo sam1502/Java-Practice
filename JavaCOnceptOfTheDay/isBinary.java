12ac7aeb84bb371fb813f902c11c4cef8727a292
Sameer Neema <sam1502@users.noreply.github.com>
2017-05-11 02:19:38 -0400

    Add files via upload

---
+++ JavaCOnceptOfTheDay/isBinary.java
@@ -0,0 +1,39 @@
+package JavaConceptOfTheDay;
+
+import java.util.Scanner;
+
+public class isBinary {
+	
+	public static void isBinary(int num){
+		int b = num;
+		boolean isBinary = true;
+		
+		while(b != 0){
+			int temp = b % 10;
+			if(temp > 1){
+				isBinary = false;
+			}
+			else
+				b = b/10; 
+			break;
+		}
+		if(isBinary){
+			System.out.println("Binary Number.");
+		}
+		else{
+			System.out.println("Not a binary number.");
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the string of numbers : ");
+		int a = in.nextInt();
+		in.close();
+		
+		isBinary(a);
+
+	}
+
+}
