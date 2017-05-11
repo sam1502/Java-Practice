71b6025643a64642243c9b6ae1d24878ccba976e
Sameer Neema <sam1502@users.noreply.github.com>
2017-05-11 02:19:14 -0400

    Add files via upload

---
+++ JavaCOnceptOfTheDay/Count_String.java
@@ -0,0 +1,26 @@
+package JavaConceptOfTheDay;
+//Simple Java Program to count the number of each charachter in the given String.
+
+import java.util.HashMap;
+
+class Count_String
+{
+    static void characterCount(String inputString)
+    {
+    	HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
+        char[] strArray = inputString.toCharArray(); 
+        for (char c : strArray)
+        {
+            if(charCountMap.containsKey(c))
+            {
+            	charCountMap.put(c, charCountMap.get(c)+1);
+            }
+            else
+            	charCountMap.put(c, 1);
+        }
+        System.out.println(charCountMap);
+    }
+ public static void main(String[] args){
+	 characterCount("Better butter");
+ }
+}
