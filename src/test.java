618b8abf8aecdef82ef90c37e2bac33ebb7428d3
sam1502 <sameer.neema02@gmail.com>
2018-10-30 22:00:55 -0500

    Codes

--- src/test.java
+++ src/test.java
@@ -1,39 +1,139 @@
-public class test{
-	
-	static int insertInSortedArr(int arr[],int n,int key, int capacity){
-		if(n >= capacity){
-			return n;
-		}
-		int i;
-		for(i=n-1; (i>0 && arr[i] > key); i--)
-			arr[i+1] = arr[i];
-		
-		arr[i+1] = key;
-		return (n+1);
-	}
-	
-	static void view(int arr[]){
-		for(int i=0;i<arr.length;i++){
-			System.out.println(arr[i]);
-		}
-	}
-	
-	
-	
-	
-	public static void main(String args[]){
-		int arr[] = new int[20];
-		arr[0] = 10;
-		arr[1] = 12;
-		arr[2] = 14;
-		arr[3] = 20;
-		arr[4] = 40;
-		arr[5] = 70;
-		
-		int capacity = arr.length;
-		int key = 26;
-		int n =6;
-		System.out.println(insertInSortedArr(arr,n,key,capacity));
-		view(arr);	
-	}
+import java.util.Scanner;
+
+import java.util.Random;
+
+public class test
+
+{
+
+public static void main(String [] args)
+
+{
+
+Scanner in = new Scanner(System.in);
+
+Random random = new Random();
+
+int choice;
+
+int passLength;
+
+int num1 = 0;
+
+char character;
+
+String password = "";
+
+System.out.println(" Password Generation Menu ");
+
+System.out.println("*****************************************************************************");
+
+System.out.println("* [1] Lowercase Letters *");
+
+System.out.println("* [2] Lowercase Letters & Uppercase Letters *");
+
+System.out.println("* [3] Lowercase Letters, Uppercase Letters, and Numbers *");
+
+System.out.println("* [4] All keyboard symbols *");
+
+System.out.println("* [5] Quit *");
+
+System.out.println("*****************************************************************************");
+
+System.out.print("\n\nEnter choice (1-5): ");
+
+choice = in.nextInt();
+
+System.out.print("Password Length (1-14): ");
+
+passLength = in.nextInt();
+
+while(password.length() < passLength)
+
+{
+
+if(choice == 1){
+
+num1 = random.nextInt(122-97) + 97;
+
+character = (char)num1;
+
+String charString = Character.toString(character);
+
+password += charString;
+
+}
+
+else if(choice == 2){
+
+num1 = random.nextInt(122-65) + 65;
+
+if((num1 > 97 && num1 < 122) || (num1 > 65 && num1 < 90))
+
+{
+
+character = (char)num1;
+
+String charString = Character.toString(character);
+
+password += charString;
+
+}
+
+}
+
+else if(choice == 3)
+
+{
+
+num1 = random.nextInt(122-48) + 48;
+
+if((num1 < 57) || (num1 > 65 && num1 < 90) || (num1 > 97 && num1 < 122))
+
+{
+
+character = (char)num1;
+
+String charString = Character.toString(character);
+
+password += charString;
+
+}
+
+else if(choice == 4)
+
+{
+
+num1 = random.nextInt(126-33) + 33;
+
+if(num1 > 33 || num1 < 126)
+
+{
+
+character = (char)num1;
+
+String charString = Character.toString(character);
+
+password += charString;
+
+}
+
+else
+
+{
+
+System.out.println("Good bye! Thank you for visiting!");
+
+}
+
+System.out.print("\nPassword: " + password);
+
+}
+
+}
+
+}
+
+}
+
 }
