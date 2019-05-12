a0c687c440441777b66dc17b6770e212b13a7b35
Sameer Neema <sameer.neema02@gmail.com>
2019-05-11 23:56:24 -0400

    updated

---
+++ src/Consecutive1TF.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Consecutive1TF {
+
+
+    public static boolean checkAppearance(String[] arr) {
+        boolean result = false;
+
+        for(int i=0;i<arr.length;i++) {
+
+        }
+
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+
+        String[] arr = new String[a];
+
+        for(int i=0;i<a;i++) arr[i] = in.next();
+
+        checkAppearance(arr);
+    }
+}
